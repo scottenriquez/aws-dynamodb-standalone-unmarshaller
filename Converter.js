523d341def1839e810b35e968296e3a1a723ff6a
Scottie Enriquez <scott.c.enriquez@gmail.com>
2018-12-23 17:24:57 -0600

    Initial implementation

--- Converter.js
+++ Converter.js
@@ -1,5 +1,66 @@
-convertDynamoDBResponseObject = (dynamoDBObject) => {
-    return "not implemented";
+output = (input) => {
+    return convertDynamoDBResponseObject({'M': input});
 };
 
-module.exports = { convertDynamoDBResponseObject };
+convertDynamoDBResponseObject = (input) => {
+    let list, map, index;
+    for (let type in input) {
+        const values = input[type];
+        if (type === 'M') {
+            map = {};
+            for (let key in values) {
+                map[key] = convertDynamoDBResponseObject(values[key]);
+            }
+            return map;
+        }
+        else if (type === 'L') {
+            list = [];
+            for (index = 0; index < values.length; index++) {
+                list.push(convertDynamoDBResponseObject(values[index]));
+            }
+            return list;
+        }
+        else if (type === 'SS') {
+            list = [];
+            for (index = 0; index < values.length; index++) {
+                list.push(values[index] + '');
+            }
+            return list;
+        }
+        else if (type === 'NS') {
+            list = [];
+            for (index = 0; index < values.length; index++) {
+                list.push(convertNumber(values[index]));
+            }
+            return list;
+        }
+        else if (type === 'BS') {
+            list = [];
+            for (index = 0; index < values.length; index++) {
+                list.push(new Buffer(values[index]));
+            }
+            return list;
+        }
+        else if (type === 'S') {
+            return values + '';
+        }
+        else if (type === 'N') {
+            return convertNumber(values);
+        }
+        else if (type === 'B') {
+            return Buffer(values);
+        }
+        else if (type === 'BOOL') {
+            return (values === 'true' || values === 'TRUE' || values === true);
+        }
+        else if (type === 'NULL') {
+            return null;
+        }
+    }
+};
+
+convertNumber = (number) => {
+    return Number(number);
+};
+
+module.exports = {output};
--- test/Converter.spec.js
+++ test/Converter.spec.js
@@ -2,15 +2,31 @@ const expect = require('chai').expect;
 const converter = require('../Converter');
 
 describe('converter', () => {
-    it('should remove type properties', () => {
+    it('should remove string type properties', () => {
         //arrange
-        const inputDynamoDBObject = {};
-        const expectedOutput = {};
+        const inputDynamoDBObject = {
+            'property1': {
+                'S': 'property1'
+            },
+            "property2": {
+                'S': 'property2'
+            },
+            'property3': {
+                'S': 'property3'
+            }
+        };
+        const expectedOutput = {
+            property1: 'property1',
+            property2: 'property2',
+            property3: 'property3',
+        };
 
         //act
-        const actualOutput = converter.convertDynamoDBResponseObject(inputDynamoDBObject);
+        const actualOutput = converter.output(inputDynamoDBObject);
+        console.log(actualOutput);
+        console.log(expectedOutput);
 
         //assert
-        expect(actualOutput).to.be.equal(expectedOutput);
+        expect(actualOutput).to.deep.equal(expectedOutput);
     });
 });
